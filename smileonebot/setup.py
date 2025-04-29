57c508606ba0e267be96134edf886c479baf924f
Cheagjihvg <88925726+Cheagjihvg@users.noreply.github.com>
2025-04-29 13:02:22 +0700

    Create setup.py

---
+++ smileonebot/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='smileone',                      # 👈 must match import name
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    author='NANG',
+    description='SmileOne API integration bot',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    python_requires='>=3.7',
+)
