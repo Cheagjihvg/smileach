a3f428e0c492defdc83552deca97577adef15806
Cheagjihvg <88925726+Cheagjihvg@users.noreply.github.com>
2025-04-29 13:24:12 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='smileone',                      # 👈 must match import name
+    name='smileach',                      # 👈 must match import name
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
